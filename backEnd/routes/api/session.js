d0cee1776b9376e10045824f5f05fbb899057808
Alex <alex.hertel87@gmail.com>
2021-06-17 16:20:01 -0500

    completed phase 4

--- backEnd/routes/api/session.js
+++ backEnd/routes/api/session.js
@@ -40,6 +40,20 @@ router.delete(
   }
 );
 
+// Restore session user
+router.get(
+  '/',
+  restoreUser,
+  (req, res) => {
+    const { user } = req;
+    if (user) {
+      return res.json({
+        user: user.toSafeObject()
+      });
+    } else return res.json({});
+  }
+);
+
 
 
 
