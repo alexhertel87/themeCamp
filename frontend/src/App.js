d6e358424359cb44d99342a1007309dc696896c3
Alex <alex.hertel87@gmail.com>
2021-06-24 17:19:25 -0500

    got reservations to render on main page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,10 @@
 // frontend/src/App.js
 import React, { useState, useEffect } from "react";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Route, Switch } from "react-router-dom";
 import * as sessionActions from "./store/session";
+import { useHistory } from "react-router";
+
 import Navigation from "./components/Navigation";
 import Footer from "./components/Footer";
 import Splash from "./components/Splash/index";
@@ -11,16 +13,31 @@ import SignupFormModal from "./components/SignupFormModal";
 import CampPageComponent from "./components/campPage";
 import CampsComponent from "./components/splashCamps";
 import ReservationsComponent from "./components/Reservations";
+import { ReserveFromUserId } from '././store/reservations';
 
 
 // import Slideshow from './components/Slideshow'
 
 function App() {
   const dispatch = useDispatch();
+  const history = useHistory();
   const [isLoaded, setIsLoaded] = useState(false);
   useEffect(() => {
     dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
   }, [dispatch]);
+
+  const user = useSelector(state => state.session.user)
+
+  useEffect(() => {
+    if (!user) {
+      history.push('/')
+    }
+  }, [history, user])
+
+  useEffect(() => {
+    dispatch(ReserveFromUserId(user?.id));
+  }, [dispatch, user])
+
 
 
   return (
